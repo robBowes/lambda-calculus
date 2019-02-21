f530f5f67282662790d2ccf8a97865f731a08065
robBowes <32183505+robBowes@users.noreply.github.com>
2019-02-21 07:24:48 -0500

    first commit

---
+++ numbers.js
@@ -0,0 +1,25 @@
+module.exports = {
+    // Create numbers from functions. Each function gets called n times
+    zero: f => x => x,
+    one: f => x => f(x),
+    two: f => x => f(f(x)),
+    three: f => x => f(f(f(x))),
+    four: f => x => f(f(f(f(x)))),
+
+    // Apply a function that increments one n times,
+    // where n is the number being converted to an int
+    toInt: f => f(i => i + 1)(0),
+
+    successor: n => f => x => f(n(f)(x)),
+
+    add: n => m => f => x => m(f)(n(f)(x)),
+
+    multiply: n => m => f => x => m(n(f))(x),
+
+    exponent: n => m => m(n),
+
+    troo: t => _ => t,
+
+    falzz: _ => f => f,
+
+}
