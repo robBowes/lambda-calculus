7bda0c328b6d3ec1ada3348413814b3fedcc17b1
robBowes <32183505+robBowes@users.noreply.github.com>
2019-02-21 23:13:54 -0500

    init package

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "no-unused-vars": "off"
+    },
+    "globals": {
+        "describe": "readonly",
+        "it": "readonly"
+    }
+};
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const numbers = require('./numbers');
+const numbers = require("./numbers")
+const {
+    toInt,
+    zero,
+    successor,
+    one,
+    two,
+    three,
+    four,
+    add,
+    multiply,
+    exponent
+} = numbers
 
-const { toInt, zero,successor, one, two, three, four, add, multiply, exponent} = numbers;
-
-
-console.assert(toInt(zero) === 0, "Zero is zero")
-console.assert(toInt(successor(zero)) === 1, "The successor of zero is 1")
-console.assert(toInt(add(one)(two)) === 3, "One plus two is 3")
-console.assert(toInt(multiply(two)(two)) === 4, "Two times two is four")
-console.assert(toInt(exponent(two)(two)) === 4, "Two exponent two is four")
-console.assert(toInt(exponent(four)(two)) === 16, "Four exponent two is sixteen")
+setInterval(() => {}, 1 << 30)
